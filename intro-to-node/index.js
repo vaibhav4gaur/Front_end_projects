453729e0780eb4ddbd5596b0404ff2e5759ff181
vaibhavgaur <110227490+vaibhav4gaur@users.noreply.github.com>
2023-03-30 21:25:21 +0530

    This is File related to JS & HTML & CSS

---
+++ intro-to-node/index.js
@@ -0,0 +1,9 @@
+//jshint version
+var supervillain = require("supervillains");
+var superheroes = require("superheroes");
+
+var mySupervillainName = supervillain.random();
+var mySuperheroName = superheroes.random();
+
+console.log(mySuperheroName);
+console.log(mySupervillainName);
